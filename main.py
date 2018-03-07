2bfb3ed44cfed82f609b7206b689e592f64d408d
zhuoju36 <zhuoju36@hotmail.com>
2018-03-08 01:53:03 +0800

    bug fixed

--- main.py
+++ main.py
@@ -7,33 +7,33 @@ from Modeler.Element import Beam,TriMembrane
 from Modeler.FEModel import FEModel
 #import Logger
 from Modeler.FEModel import solve_linear
+import numpy as np
 
-
-if __name__=='__main__':
+def simple_supported_beam_test():
     #FEModel Test
     model=FEModel()
     n1=Node(0,0,0)
     n2=Node(1,0,0)
     n3=Node(2,0,0)
     b1=Beam(n1,n2,2e5,0.3,0.013,2.675e-5,3.435e-4,1.321e-6,7.85)
-    b2=Beam(n1,n2,2e5,0.3,0.013,2.675e-5,3.435e-4,1.321e-6,7.85)
+    b2=Beam(n2,n3,2e5,0.3,0.013,2.675e-5,3.435e-4,1.321e-6,7.85)
     model.add_node(n1)
     model.add_node(n2)
     model.add_node(n3)
     model.add_beam(b1)
     model.add_beam(b2)
-    n2.Fn=(0,0,-1,0,0,0)
-    n1.restraint=[True]*6
-    n3.restraint=[True]*6
+    n2.fn=(0,0,-1,0,0,0)
+    n1.dn=[0,0,0,0,0,0]
+    n3.dn=[0,0,0,0,0,0]
+    b1.releases=[False]*3+[True]*3+[False]*6
+    b2.releases=[False]*6+[False]*3+[True]*3
     model.assemble_KM()
-    model.assemble_FD()
-    model.eliminate_matrix()
+    model.assemble_f()
+    model.assemble_boundary()
     res=solve_linear(model)
-    print(res)
-    
-    
+    print(np.round(res,6))
 
-
+simple_supported_beam_test()
 #    from random import random
 #    model=FEModel()
 #    for i in range(333):
