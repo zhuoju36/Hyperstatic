619e7cdf7f6b68c5830d7b94f3e7db76929c2e96
HZJ <zhuoju36@hotmail.com>
2022-06-30 09:40:06 +0800

    bug fixed

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup
 from setuptools import find_packages
 
-<<<<<<< HEAD
-VERSION = '0.1.11'
-=======
 
 VERSION = '0.1.12'
->>>>>>> 01a10ef3699b5dfa5af6611afddaa149cb9f5fff
 
 setup(
     name='StructEngPy', 
@@ -21,10 +17,6 @@ setup(
     install_requires=[
         'numpy>=1.21',
         'scipy>=1.7',
-<<<<<<< HEAD
-        'quadpy>=0.16',
-=======
         'quadpy>=0.16'
->>>>>>> 01a10ef3699b5dfa5af6611afddaa149cb9f5fff
     ],
 )
