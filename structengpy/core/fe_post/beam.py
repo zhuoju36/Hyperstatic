682552dc455b3875716b5347aec1bf855f71216e
HZJ <zhuoju36@hotmail.com>
2022-07-06 19:10:37 +0800

    update test

--- structengpy/core/fe_post/beam.py
+++ structengpy/core/fe_post/beam.py
@@ -24,8 +24,8 @@ class BeamResultResolver(ResultResolver):
         res = K*dd
         return res
 
-    def resolve_beam_displacement(self,name,casename:str,step:int=1,loc=0.5):
-        path=self.__path
+    def resolve_beam_displacement(self,name,loc,casename:str,step:int=1):
+        path=self.__workpath
         d=np.load(os.path.join(path,casename+".d.npy"))[step-1,:] # row-first
         hids=self.__assembly.get_beam_node_hids(name)
         dd=[d[i] for i in hids]
@@ -42,6 +42,41 @@ class BeamResultResolver(ResultResolver):
             a[0],b[0],c[0],d[0],b[1],c[1],
             a[1],b[2],c[2],d[1],b[3],c[3]
         ])
+if __name__=="__main__":
+    import logging
+    import sys
+    import os
+
+    from structengpy.core.fe_model.assembly import Assembly
+    from structengpy.core.fe_model.model import Model
+    from structengpy.core.fe_model.load.pattern import LoadPattern
+    from structengpy.core.fe_model.load.loadcase import ModalCase, StaticCase
+    from structengpy.core.fe_solver.dynamic import ModalSolver
+    from structengpy.core.fe_solver.static import StaticSolver
+    path="./test"
+    if sys.platform=="win32":
+        path="c:\\test"
+
+    model=Model()
+    model.add_node("1",0,0,0)
+    model.add_node("2",6,0,0)
+    model.add_beam("A","1","2",E=2e11,mu=0.3,A=0.0188,I2=4.023e-5,I3=4.771e-4,J=4.133e-6,rho=7.85e10)
+
+    patt1=LoadPattern("pat1")
+    patt1.set_nodal_load("2",0,0,-1e4,0,0,0)
+
+    lc=StaticCase("case1")
+    lc.add_pattern(patt1,1.0)
+    lc.set_nodal_restraint("1",True,True,True,True,True,True)
+    asb=Assembly(model,[lc])
+    asb.save(path,"test.asb")
+    solver=StaticSolver(path,"test.asb")
+    solver.solve_linear("case1")
+    
+    resolver=BeamResultResolver(path,"test.asb")
+    d=resolver.resolve_beam_displacement("A",0.5,"case1")
+    print(d)
+    
 
 
         
--- tests/test_core/test_post/test_beam.py
+++ tests/test_core/test_post/test_beam.py
@@ -13,11 +13,11 @@ from structengpy.core.fe_model.model import Model
 from structengpy.core.fe_model.load.pattern import LoadPattern
 from structengpy.core.fe_model.load.loadcase import ModalCase, StaticCase
 from structengpy.core.fe_solver.dynamic import ModalSolver
-from structengpy.core.fe_post.node import NodeResultResolver
+from structengpy.core.fe_post.beam import BeamResultResolver
 from structengpy.core.fe_solver.static import StaticSolver
 
 
-class TestNodeResult():
+class TestBeamResult():
     def test_static(self):
         path="./test"
         if sys.platform=="win32":
@@ -39,32 +39,11 @@ class TestNodeResult():
         solver=StaticSolver(path,"test.asb")
         solver.solve_linear("case1")
         
-        resolver=NodeResultResolver(path,"test.asb")
-        d=resolver.resolve_nodal_displacement("2","case1")
+        resolver=BeamResultResolver(path,"test.asb")
+        d=resolver.resolve_beam_displacement("A",0.5,"case1")
+        print(d)
+        assert d==1
 
-        assert d[2]==approx(-0.00764,rel=5e-2)
-        assert d[4]==approx(0.00189,rel=5e-2)
 
-    def test_mode(self):
-        path="./test"
-        if sys.platform=="win32":
-            path="c:\\test"
 
-        model=Model()
-        model.add_node("1",0,0,0)
-        model.add_node("2",6,0,0)
-        model.add_beam("A","1","2",E=2e11,mu=0.3,A=0.0188,I2=4.023e-5,I3=4.771e-4,J=4.133e-6,rho=7.85e10)
-        model.set_nodal_mass("2",1,1,1,1,1,1)
-
-        lc=ModalCase("eigen")
-        lc.use_load_as_mass=False
-        lc.set_nodal_restraint("1",True,True,True,True,True,True)
-        asb=Assembly(model,[lc])
         
-        asb.save(path,"test.asb")
-        solver=ModalSolver(path,"test.asb")
-        solver.solve_eigen("eigen",3)
-
-        resolver=NodeResultResolver(path,"test.asb")
-        resolver.resolve_nodal_displacement("2","eigen",1)
-   
